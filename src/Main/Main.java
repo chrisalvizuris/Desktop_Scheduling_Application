e92182e3333df2b494e9bb0d9d53dc35aa45233d
chris alvizuris <chris.alvizuris@gmail.com>
2021-01-15 22:06:42 -0800

    Began login screen FXML

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/loginForm.fxml"));
+        primaryStage.setTitle("Scheduler");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
