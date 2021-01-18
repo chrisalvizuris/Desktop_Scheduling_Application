6c230b5927c794937b32bfe1ff1ef12ce8dfaee4
chris alvizuris <chris.alvizuris@gmail.com>
2021-01-17 22:12:58 -0800

    Started working on main window. Added tabs.

---
+++ src/Model/Countries.java
@@ -0,0 +1,15 @@
+//package Model;
+//
+//import java.util.Date;
+//
+//public class Countries {
+//
+//    private int countryID;
+//    private String countryName;
+//    private Date countryCreateDate;
+//    private String countryCreatedBy;
+//    private Date countryLastUpdate;
+//    private String countryLastUpdateBy;
+//
+//    public static Countries {}
+//}
