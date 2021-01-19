ec140bcfc2208cfe35c1e0cae38e63e6f71cf44f
chris alvizuris <chris.alvizuris@gmail.com>
2021-01-18 16:36:26 -0800

    Created appointment page. Updated new customer. Linked pages.

--- src/Controller/addCustomerFormController.java
+++ src/Controller/addCustomerFormController.java
@@ -1,4 +1,31 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class addCustomerFormController {
+
+    @FXML
+    private AnchorPane divisionComboBox;
+
+    @FXML
+    private TextField customerIdTextField;
+
+    @FXML
+    private TextField customerNameTextField;
+
+    @FXML
+    private TextField customerAddressTextField;
+
+    @FXML
+    private TextField customerPostalCodeTextField;
+
+    @FXML
+    private TextField customerPhoneTextField;
+
+    @FXML
+    private ComboBox<?> countryComboBox;
+
 }
--- src/Controller/mainWindowController.java
+++ src/Controller/mainWindowController.java
@@ -1,4 +1,30 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class mainWindowController {
+
+    @FXML
+    private Button newAppointmentButton;
+
+    @FXML
+    private void newAppointmentButtonPushed(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/View/addAppointmentForm.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+    }
 }
