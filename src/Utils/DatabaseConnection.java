e6d0249883ff96b4f77d7199011fa5e2577cadef
chris alvizuris <chris.alvizuris@gmail.com>
2021-01-30 13:32:19 -0800

    Created DatabaseConnection class and connected the database.

---
+++ src/Utils/DatabaseConnection.java
@@ -0,0 +1,41 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    //setting up jdbc url with strings
+    private static final String protocol = "jdbc";
+    private static final String vendorName = ":mysql:";
+    private static final String ipAddress = "//wgudb.ucertify.com/WJ05zKd";
+
+    //jdbc url
+    private static final String jdbcURL = protocol + vendorName + ipAddress;
+
+    //driver and connection interface reference
+    private static final String MYSQLJDBCDriver = "com.mysql.cj.jdbc.Driver";
+    private static Connection conn = null;
+
+    //username
+    private static final String username = "U05zKd";
+
+    //password
+    private static final String password = "53688653713";
+
+    public static Connection beginConnection() {
+        try {
+            Class.forName(MYSQLJDBCDriver);
+            conn = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connection Successful");
+        }   catch (ClassNotFoundException exception) {
+            System.out.println(exception.getMessage());
+        }   catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module Desktop.Scheduling.Application {
 
     requires javafx.controls;
     requires javafx.fxml;
+    requires mysql.connector.java;
+    requires java.sql;
 
     opens View;
     opens Main;
